470bebc31fdac09e4bf32f4f3f67d02ea28371be
⋆｡‧˚ʚ🍓ɞ˚‧｡⋆ <emanuely.tinti@escola.pr.gov.br>
2026-05-21 12:27:18 -0300

    uuu

---
+++ main.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+// Opcional: Fecha o menu ao clicar em qualquer link
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
